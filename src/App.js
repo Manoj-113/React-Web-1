7b9e8c093087f61ee4692cdaf207262995a9ccc8
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-16 18:33:37 +0530

    Create App function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -30,7 +30,12 @@ const theme = createMuiTheme({
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <ThemeProvider theme={theme}>
+
+      </ThemeProvider>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -43,7 +48,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
