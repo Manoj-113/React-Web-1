c3065e34e398700cb378225b82ed9e829bbef337
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-18 13:56:28 +0530

    Make Grid function in Grid.js file

---
+++ src/components/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+import CustomBtn from './CustomBtn'
+
+ function Grid() {
+    return (
+        
+    )
+}
+
+
+export default Grid
