116fb187a825c2048e029af3dada820c5357145d
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-18 13:48:57 +0530

    Adding Css for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,42 @@ import {makeStyles} from '@material-ui/styles';
 import logo from '../img/1.png';
 import logoMobile from '../img/2.png';
 
+const styles = makeStyles({
+    bar:{
+        paddingTop:"1.15rem",
+        backgroundColor:"#fff",
+        ['@media (max-width:780px)']:{
+            flexDirection:"column"
+        }
+    },
+    logo:{
+        width:"15%",
+        ['@media (max-width:780px)']:{
+            display:"none"
+        }
+    },
+    logoMobile:{
+        width:"100%",
+        display:"none",
+        ['@media (max-width:780px)']:{
+            display:"inline-none"
+        }
+    },
+    menuItem:{
+        cursor:"pointer",
+        flexGrow: 1,
+        "&:hover":{
+            color:"#4f25c8"
+        },
+        ['@media (max-width:780px)']:{
+            paddingBottom:"1rem"
+        }
+    }
+})
+
+
 function NavBar() {
-
     const classes = styles()
-
-
     return (
         <Toolbar position = "sticky" color = "rgba(0,0,0,0.87)" className={classes.bar}>
             <img src = {logo} className = {classes.logo}/>
