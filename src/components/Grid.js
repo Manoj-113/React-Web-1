50c4e4f06ca52452e02801610316849ef1f8a55e
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-19 13:34:53 +0530

    const styles

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,6 +3,19 @@ import {Typography} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import CustomBtn from './CustomBtn'
 
+const styles = makeStyles({
+    wrapper: {
+        display: "flex",
+        flexDirection : "column",
+        alignItems : "center",
+        padding : "0 5rem 0 5rem"
+    },
+    item: {
+        paddingTop:"1rem"
+    }
+})
+
+
  function Grid() {
     return (
         
