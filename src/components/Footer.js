fd5e2a0c4413aed7870637977fe5758aa3f6016c
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-05-21 23:09:45 +0530

    ImportIcons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {BottomNavigation,BottomNavigationActions} from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Instagram from '@material-ui/icons/Instagram';
+import YouTube from '@material-ui/icons/YouTube';
 
  function Footer() {
     return (
